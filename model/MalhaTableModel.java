5bafbfcabd19f66e67fe500be7c0ab44a719e742
Max Singular <max.starke@singular.inf.br>
2023-10-01 14:29:05 -0300

    table model gerando a malha

--- model/MalhaTableModel.java
+++ model/MalhaTableModel.java
@@ -2,19 +2,51 @@ package model;
 
 import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class MalhaTableModel extends AbstractTableModel {
 
     private Celula matrizMalha[][];
 
+    private File arquivoMalha;
     private int qtdLinhas;
     private int qtdColunas;
+    private Scanner matrizScanner;
 
-
-    public MalhaTableModel(int qtdLinhas, int qtdColunas) {
-        this.qtdLinhas = qtdLinhas;
-        this.qtdColunas = qtdColunas;
+    public MalhaTableModel() {
+        this.inicializarVariaveis();
+        this.inicializarMalha();
     }
+
+    private void inicializarVariaveis(){
+        this.arquivoMalha = new File(Config.getInstance().getMalhaAtual());
+        try{
+            matrizScanner = new Scanner(arquivoMalha);
+            this.qtdLinhas = matrizScanner.nextInt();
+            this.qtdColunas = matrizScanner.nextInt();
+            this.matrizMalha = new Celula[qtdLinhas][qtdColunas];
+
+        }catch (Exception e){
+            System.out.println(e.getMessage()+" - "+ Arrays.toString(e.getStackTrace()));
+        }
+    }
+
+    private void inicializarMalha(){
+
+        for (int linha = 0; linha < this.qtdLinhas; linha++) {
+            for (int coluna = 0; coluna < this.qtdColunas; coluna++) {
+                while (matrizScanner.hasNextInt()){
+                    int tipo = matrizScanner.nextInt();
+                    Celula celulaAtual = new Celula(coluna, linha, tipo);
+                    this.matrizMalha[coluna][linha] = celulaAtual;
+                }
+            }
+        }
+    }
+
 
     @Override
     public int getRowCount() {
